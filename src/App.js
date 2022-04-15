648d6955241cbc813b2cdd4650bdab1083812472
Christian-pprogrammer <mpanoc6@gmail.com>
2022-04-15 19:10:48 +0200

    update app.js for the useRef example 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import UseRefExample1 from "./components/UseRefExample1";
+import UseRefExample2 from "./components/UseRefExample2";
 
 function App() {
   return (
     <div className="container">
       <h1 className="display-1 text-center">React advanced hooks</h1>
-      <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 }
